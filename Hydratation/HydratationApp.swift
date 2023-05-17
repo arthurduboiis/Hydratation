a19f064803c658ce304c6113a7e440d74cc7f7a9
Arthur dubois <arthur.dbs70@gmail.com>
2023-05-17 14:20:25 +0200

    Initial Commit

---
+++ Hydratation/HydratationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydratationApp.swift
+//  Hydratation
+//
+//  Created by Arthur dubois on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HydratationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
